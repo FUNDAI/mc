9a6884dcf9f3d027e5bd46ee3edc5d702278483e
FUNDAI <ethan@jaiohsieh.com>
2022-02-09 16:20:08 +0800

    command base v6

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.LimelightAim;
@@ -15,14 +13,12 @@ public class RobotContainer {
   public static Joystick js1 = new Joystick(Constants.js1);
 
   public RobotContainer() {
+    configureButtonBindings();
     drivesubsystem.setDefaultCommand(new Mdrive(drivesubsystem, 
     () -> js1.getRawAxis(Constants.leftStick_Y), 
     () -> js1.getRawAxis(Constants.leftStick_X), 
     () -> js1.getRawAxis(Constants.rightStick_X)));
     drivesubsystem.setDefaultCommand(new LimelightAim(drivesubsystem,0,0,0));
-    
-    configureButtonBindings();
-    
   }
 
   private void configureButtonBindings() {
